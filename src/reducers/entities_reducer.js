136afcc6ce9a9459f71b39ebcef1c7e79f37d948
bismuth1991 <pbao.tran.unsw@gmail.com>
2019-01-20 23:18:53 -0500

    Create entitiesReducer with songs, artists and albums

---
+++ src/reducers/entities_reducer.js
@@ -0,0 +1,13 @@
+import { combineReducers } from 'redux';
+
+import songsReducer from './songs_reducer';
+import artistsReducer from './artists_reducer';
+import albumsReducer from './albums_reducer';
+
+const entitiesReducer = combineReducers({
+  songs: songsReducer,
+  artists: artistsReducer,
+  albums: albumsReducer,
+});
+
+export default entitiesReducer;
