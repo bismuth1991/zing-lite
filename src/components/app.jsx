7a0f84b61240315c5109a0e870d74f8775addb39
bismuth1991 <pbao.tran.unsw@gmail.com>
2019-01-22 22:17:02 -0800

    Make custom media break

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -7,11 +7,7 @@ import AudioPlayerContainer from './audio_player/audio_player_container';
 
 const App = () => (
   <>
-    {/* <div style={{ position: 'sticky', top: '0' }}> */}
     <nav className="HolyGrail-nav">Navigation</nav>
-    {/* <div style={{ padding: '10px', backgroundColor: 'white' }} /> */}
-    {/* </div> */}
-
 
     <main className="HolyGrail-body">
       <article className="HolyGrail-content">
