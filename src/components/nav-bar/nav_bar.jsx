aa18d5d4b0997efc5cc16bc8c615955a8269226b
bismuth1991 <pbao.tran.unsw@gmail.com>
2019-01-24 13:34:55 -0800

    Add activeSyle to NavLink

--- src/components/nav-bar/nav_bar.jsx
+++ src/components/nav-bar/nav_bar.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
+const activeStyle = {
+  color: 'white',
+};
+
 const NavBar = () => (
   <ul className="Grid Grid--center Grid--justifySpaceAround" style={{ height: '2em' }}>
-    <NavLink to="/home" className="no-margin-bottom">Home</NavLink>
-    <NavLink to="/playlist" className="no-margin-bottom">Playlist</NavLink>
-    <NavLink to="/profile" className="no-margin-bottom">Profile</NavLink>
+    <NavLink to="/home" activeStyle={activeStyle} className="no-margin-bottom">Home</NavLink>
+    <NavLink to="/playlist" activeStyle={activeStyle} className="no-margin-bottom">Playlist</NavLink>
+    <NavLink to="/profile" activeStyle={activeStyle} className="no-margin-bottom">Profile</NavLink>
   </ul>
 );
 
