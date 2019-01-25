ee0b0cbf7e652d54a9d39b07b6b4b2eb53b49377
bismuth1991 <pbao.tran.unsw@gmail.com>
2019-01-25 09:19:55 -0800

    Create SignupFormContainer

---
+++ src/components/main/session_form/signup_form_container.js
@@ -0,0 +1,20 @@
+import { connect } from 'react-redux';
+
+import { signup } from '../../../actions/session_actions';
+import SessionForm from './session_form';
+
+const mapStateToProps = ({ errors }) => ({
+  errors: errors.session,
+  formType: 'SIGN UP',
+});
+
+const mapDispatchToProps = dispatch => ({
+  processForm: user => dispatch(signup(user)),
+});
+
+const SignupFormContainer = connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(SessionForm);
+
+export default SignupFormContainer;
