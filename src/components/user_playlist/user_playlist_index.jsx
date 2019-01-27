75d5036dd638c1854f913874ae64bf561fb56aae
bismuth1991 <pbao.tran.unsw@gmail.com>
2019-01-27 02:28:24 -0500

    Add userPlaylistItemContainer

---
+++ src/components/user_playlist/user_playlist_index.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import UserPlaylistItem from './user_playlist_item';
+
+class UserPlaylistIndex extends React.Component {
+  componentDidMount() {
+    const { fetchUserPlaylists } = this.props;
+    fetchUserPlaylists();
+  }
+
+  componentDidUpdate() {
+
+  }
+
+  render() {
+    const {
+      userPlaylists,
+      currentUser,
+      fetchUserPlaylist,
+      addSongsToList,
+      deleteUserPlaylist,
+    } = this.props;
+
+    if (currentUser.id === null) return null;
+    if (userPlaylists.length === 0) return null;
+
+    return (
+      <section className="section">
+        <div className="container">
+          <h2>Personal Playlists</h2>
+          <ul className="grid grid-gutter padding-left" id="playlist-index">
+            {userPlaylists.map(playlist => (
+              <li className="grid-cell u-full u-med-1of2 u-large-1of3 u-xlarge-1of4" key={playlist.id}>
+                <UserPlaylistItem
+                  playlistName={playlist.name}
+                  playlistId={playlist.id}
+                  fetchUserPlaylist={fetchUserPlaylist}
+                  addSongsToList={addSongsToList}
+                  deleteUserPlaylist={deleteUserPlaylist}
+                />
+              </li>
+            ))}
+          </ul>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default UserPlaylistIndex;
---
+++ src/components/user_playlist/user_playlist_item_container.js
@@ -0,0 +1,18 @@
+import { connect } from 'react-redux';
+
+import { deleteUserPlaylist } from '../../actions/user_playlist_actions';
+import { startPlaylist } from '../../actions/audio_player_actions';
+
+import UserPlaylistItem from './user_playlist_index';
+
+const mapDispatchToProps = dispatch => ({
+  addSongsToList: songIds => dispatch(startPlaylist(songIds)),
+  deleteUserPlaylist: playlistId => dispatch(deleteUserPlaylist(playlistId)),
+});
+
+const UserPlaylistContainer = connect(
+  null,
+  mapDispatchToProps,
+)(UserPlaylistItem);
+
+export default UserPlaylistContainer;
