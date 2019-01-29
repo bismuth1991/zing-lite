7d40201f5c2172208e3a4268f340aaa1a8042822
bismuth1991 <pbao.tran.unsw@gmail.com>
2019-01-29 12:58:33 -0800

    Updates

---
+++ precache-manifest.6437e671b8392e15d4ad6cb787fb7946.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "30a44afc926a03539637",
+    "url": "/static/js/main.30a44afc.chunk.js"
+  },
+  {
+    "revision": "9374c737484b7e0984be",
+    "url": "/static/js/1.9374c737.chunk.js"
+  },
+  {
+    "revision": "30a44afc926a03539637",
+    "url": "/static/css/main.34986479.chunk.css"
+  },
+  {
+    "revision": "9374c737484b7e0984be",
+    "url": "/static/css/1.df87d98a.chunk.css"
+  },
+  {
+    "revision": "39426a3d2b243768d4a4dfa8812fc0a4",
+    "url": "/index.html"
+  }
+];
