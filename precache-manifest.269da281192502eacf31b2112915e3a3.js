8df177a39c9646e51897cba929a88765e63dc521
bismuth1991 <pbao.tran.unsw@gmail.com>
2019-01-30 10:10:05 -0800

    Updates

--- precache-manifest.269da281192502eacf31b2112915e3a3.js
+++ precache-manifest.269da281192502eacf31b2112915e3a3.js
@@ -4,23 +4,23 @@ self.__precacheManifest = [
     "url": "/zing-lite/static/js/runtime~main.2652eacf.js"
   },
   {
-    "revision": "379fe756c4d8e96d9364",
-    "url": "/zing-lite/static/js/main.379fe756.chunk.js"
+    "revision": "a99371d85d808655b964",
+    "url": "/zing-lite/static/js/main.a99371d8.chunk.js"
   },
   {
     "revision": "ed8c3155ea72d2ccfe60",
     "url": "/zing-lite/static/js/1.ed8c3155.chunk.js"
   },
   {
-    "revision": "379fe756c4d8e96d9364",
-    "url": "/zing-lite/static/css/main.21f6833e.chunk.css"
+    "revision": "a99371d85d808655b964",
+    "url": "/zing-lite/static/css/main.5b7bfc9f.chunk.css"
   },
   {
     "revision": "ed8c3155ea72d2ccfe60",
     "url": "/zing-lite/static/css/1.df87d98a.chunk.css"
   },
   {
-    "revision": "46a0fec71436ea0c385677ca4cf98489",
+    "revision": "66ab847ebe967f69932860719c236ec9",
     "url": "/zing-lite/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/zing-lite/precache-manifest.a781fea41abfb4eae40e39558e9fdd10.js"
+  "/zing-lite/precache-manifest.269da281192502eacf31b2112915e3a3.js"
 );
 
 workbox.clientsClaim();
