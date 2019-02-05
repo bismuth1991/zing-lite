0979e31461d5e94919957aa25a0575bf22a94082
bismuth1991 <pbao.tran.unsw@gmail.com>
2019-02-05 10:25:03 -0800

    Add content to About component

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
-import LoadIcon from '../utils/loading_icon';
 
 const About = () => (
-  <LoadIcon loading />
+  <>
+    <h4 style={{ textAlign: 'center' }}>
+      <span>Designed by: Bao Tran </span>
+      <br />
+      <a href="https://github.com/bismuth1991"><i className="fab fa-github" /></a>
+      <span>{' '}</span>
+      <a href="https://www.linkedin.com/in/baotran-91/"><i className="fab fa-linkedin" /></a>
+    </h4>
+
+    <ul>
+      <strong>Technologies used:</strong>
+      <li>JavaScript</li>
+      <li>Ruby</li>
+      <li>Ruby on Rails</li>
+      <li>PostgreSQL</li>
+      <li>React/Redux</li>
+      <li>HTML5/CSS3</li>
+    </ul>
+  </>
 );
 
 export default About;
